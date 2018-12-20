960039693f480f951948708a7c3e1c8286db476d
Trololol94 <mali3355>
2018-12-20 14:36:20 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express'),
+http = require('http'),
+app = express(),
+server = http.createServer(app),
+io = require('socket.io').listen(server);
+app.get('/', (req, res) => {
+res.send('Chat Server is running on port 3000')
+});
+
+io.on('connection', (socket) => {
+
+    console.log('user connected')
+    
+    socket.on('join', function(){
+        //console.log('Test message by server')
+        let  message = {"message":"decollage"}
+
+        setTimeout(function () {
+        socket.emit('newmessage', message)
+            }, 5000); 
+
+        //socket.emit('newmessage', message)
+    });
+
+    socket.on('messagedetection', (senderNickname,messageContent) => {
+    
+        //log the message in console 
+    
+        console.log(senderNickname+" :" +messageContent)
+            //create a message object
+    
+        let  message = {"message":messageContent, "senderNickname":senderNickname}
+        //let message = {"message": "toto", "senderNickname":senderNickname}    
+        // send the message to the client side  
+    
+        socket.broadcast.emit('message', message )
+        
+    });
+    socket.on('disconnect', function() {
+        console.log( 'user has left ')
+        socket.broadcast.emit( "userdisconnect" ,' user has left')
+    
+    
+    });
+    
+});
+
+server.listen(3000,()=>{
+console.log('Node app is running on port 3000')
+});
