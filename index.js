a4f39e9936c47849c786e982efb714407f68046a
fmeziane <farid.meziane@live.fr>
2018-12-28 13:52:30 +0100

    tableau

--- index.js
+++ index.js
@@ -7,26 +7,54 @@ app.get('/', (req, res) => {
 res.send('Chat Server is running on port 3000')
 });
 
+
+
+
 io.on('connection', (socket) => {
 
     console.log('user connected')
     
     socket.on('join', function(){
         //console.log('Test message by server')
-        let  message = {"message":"yawLeft"}
+       
+        let i=0;
+
+        let messages = [];
+        messages[0] = "takeOff";
+        messages[1] = "takeOff";
+        messages[2] = "yawLeft";
+        messages[3] = "yawLeft";
+        messages[4] = "yawLeft";
+        messages[5] = "yawLeft";
+        messages[6] = "landing";
+
+        function intervalFunc() {
+        let  message = {"message":messages[i]}
+        console.log(message)
+
+        i++;
+        socket.emit('newmessage', message);
+        if (i == '5') {
+                clearInterval(this);     
+            }
 
 
+        }
 
+        setInterval(intervalFunc, 10000);
+/*
         let check = setInterval(function () {
-        socket.emit('newmessage', message)
-            }, 1000); 
+        socket.emit('newmessage', messages[i]);
         
+            }, 5000); 
+        //console.log('user connected 2')
+
         socket.on('disconnect', function() {
         console.log( 'user has left ')
         clearInterval(check)
     
     
-    });
+    }); */
         
     });
 
